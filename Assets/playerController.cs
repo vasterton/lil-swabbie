ee408c5e2bf64382afee547d18a1ae80e1917ed3
evancmiller <evanmiller@cedarville.edu>
2019-11-13 23:45:56 -0500

    Added cook minigame and assets for ship interior

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class playerController : MonoBehaviour
 {
     NavMeshAgent player;
+    bool cookGameStarted = false;
+    bool fishHeld = false;
+    bool fishReturned = false;
+    bool cheeseHeld = false;
+    bool cheeseReturned = false;
+    bool breadHeld = false;
+    bool breadReturned = false;
 
     // Start is called before the first frame update
     void Start()
@@ -35,10 +43,102 @@ public class playerController : MonoBehaviour
 
         // Enable or disable walking animation based on magnitude of velocity
         animator.SetBool("walking", player.velocity.magnitude > 0.2f);
+
+        // Press E to interact
+        if(Input.GetKeyDown(KeyCode.E)){
+            // Begin cook minigame - this will probably be handled differently once interactions are implemented
+            if(GameObject.FindGameObjectWithTag("cookMinigameText").GetComponent<Text>().enabled){
+                cookGameStarted = true;
+
+                GameObject[] fishObjects = GameObject.FindGameObjectsWithTag("fish");
+                foreach(GameObject fish in fishObjects){
+                    fish.GetComponent<MeshRenderer>().enabled = true;
+                }
+
+                GameObject.FindGameObjectWithTag("cheese").GetComponent<MeshRenderer>().enabled = true;
+
+                GameObject.FindGameObjectWithTag("bread").GetComponent<MeshRenderer>().enabled = true;
+                
+                GameObject.FindGameObjectWithTag("cookMinigameText").GetComponent<Text>().enabled = false;
+            }
+            // Interact with ingredients
+            else if(GameObject.FindGameObjectWithTag("fishText").GetComponent<Text>().enabled){
+                fishHeld = true;
+
+                GameObject[] fishObjects = GameObject.FindGameObjectsWithTag("fish");
+                foreach(GameObject fish in fishObjects){
+                    fish.GetComponent<MeshRenderer>().enabled = false;
+                }
+
+                GameObject[] heldFishObjects = GameObject.FindGameObjectsWithTag("fishHeld");
+                foreach(GameObject fish in heldFishObjects){
+                    fish.GetComponent<MeshRenderer>().enabled = true;
+                }
+
+                GameObject.FindGameObjectWithTag("fishText").GetComponent<Text>().enabled = false;
+            }
+            else if(GameObject.FindGameObjectWithTag("cheeseText").GetComponent<Text>().enabled){
+                cheeseHeld = true;
+
+                GameObject.FindGameObjectWithTag("cheese").GetComponent<MeshRenderer>().enabled = false;
+
+                GameObject.FindGameObjectWithTag("cheeseHeld").GetComponent<MeshRenderer>().enabled = true;
+
+                GameObject.FindGameObjectWithTag("cheeseText").GetComponent<Text>().enabled = false;
+            }
+            else if(GameObject.FindGameObjectWithTag("breadText").GetComponent<Text>().enabled){
+                breadHeld = true;
+
+                GameObject.FindGameObjectWithTag("bread").GetComponent<MeshRenderer>().enabled = false;
+
+                GameObject.FindGameObjectWithTag("breadHeld").GetComponent<MeshRenderer>().enabled = true;
+
+                GameObject.FindGameObjectWithTag("breadText").GetComponent<Text>().enabled = false;
+            }
+            else if(GameObject.FindGameObjectWithTag("ingredientText").GetComponent<Text>().enabled){
+                if(fishHeld){
+                    fishHeld = false;
+                    fishReturned = true;
+
+                    GameObject[] heldFishObjects = GameObject.FindGameObjectsWithTag("fishHeld");
+                    foreach(GameObject fish in heldFishObjects){
+                        fish.GetComponent<MeshRenderer>().enabled = false;
+                    }
+
+                    GameObject[] returnedFishObjects = GameObject.FindGameObjectsWithTag("fishReturned");
+                    foreach(GameObject fish in returnedFishObjects){
+                        fish.GetComponent<MeshRenderer>().enabled = true;
+                    }
+                }
+                else if(cheeseHeld){
+                    cheeseHeld = false;
+                    cheeseReturned = true;
+
+                    GameObject.FindGameObjectWithTag("cheeseHeld").GetComponent<MeshRenderer>().enabled = false;
+
+                    GameObject.FindGameObjectWithTag("cheeseReturned").GetComponent<MeshRenderer>().enabled = true;
+                }
+                else if(breadHeld){
+                    breadHeld = false;
+                    breadReturned = true;
+
+                    GameObject.FindGameObjectWithTag("breadHeld").GetComponent<MeshRenderer>().enabled = false;
+
+                    GameObject.FindGameObjectWithTag("breadReturned").GetComponent<MeshRenderer>().enabled = true;
+                }
+
+                GameObject.FindGameObjectWithTag("ingredientText").GetComponent<Text>().enabled = false;
+
+                if(fishReturned && cheeseReturned && breadReturned){
+                    // Cook minigame is complete - handle interactions with chef here
+                }
+            }
+        }
     }
 
-    // Change camera position when lil swabbie moves to a different area of the ship
+    
     void OnTriggerEnter(Collider other){
+        // Change camera position when lil swabbie moves to a different area of the ship
         if(other.CompareTag("upperDeck")){
             Camera.main.transform.SetPositionAndRotation(new Vector3(-8.17f, 11.41f, -21.76f), Quaternion.Euler(new Vector3(20, 0, 0)));
         }
@@ -59,6 +159,41 @@ public class playerController : MonoBehaviour
         }
         else if(other.CompareTag("galley")){
             Camera.main.transform.SetPositionAndRotation(new Vector3(-12, 2.5f, -15.71f), Quaternion.Euler(new Vector3(10, 20, 0)));
+
+            if(!cookGameStarted){
+                GameObject.FindGameObjectWithTag("cookMinigameText").GetComponent<Text>().enabled = true;
+            }
+            else if(fishHeld || cheeseHeld || breadHeld){
+                GameObject.FindGameObjectWithTag("ingredientText").GetComponent<Text>().enabled = true;
+            }
+        }
+        // If cook minigame has started, show text when lil swabbie moves near an ingredient
+        else if(cookGameStarted){
+            if(other.CompareTag("fish") && !fishReturned && !cheeseHeld && !breadHeld){
+                GameObject.FindGameObjectWithTag("fishText").GetComponent<Text>().enabled = true;
+            }
+            else if(other.CompareTag("cheese") && !cheeseReturned && !fishHeld && !breadHeld){
+                GameObject.FindGameObjectWithTag("cheeseText").GetComponent<Text>().enabled = true;
+            }
+            else if(other.CompareTag("bread") && !breadReturned && !cheeseHeld && !fishHeld){
+                GameObject.FindGameObjectWithTag("breadText").GetComponent<Text>().enabled = true;
+            }
+        }
+    }
+
+    // Hide text when lil swabbie leaves certain areas
+    void OnTriggerExit(Collider other){
+        if(other.CompareTag("galley")){
+            GameObject.FindGameObjectWithTag("cookMinigameText").GetComponent<Text>().enabled = false;
+        }
+        else if(other.CompareTag("fish")){
+            GameObject.FindGameObjectWithTag("fishText").GetComponent<Text>().enabled = false;
+        }
+        else if(other.CompareTag("cheese")){
+            GameObject.FindGameObjectWithTag("cheeseText").GetComponent<Text>().enabled = false;
+        }
+        else if(other.CompareTag("bread")){
+            GameObject.FindGameObjectWithTag("breadText").GetComponent<Text>().enabled = false;
         }
     }
 }
